d3a05457c245906a05f00d54254da178a79f984c
David Bohn <davbohn@googlemail.com>
2015-07-10 16:30:26 +0200

    Initial Commit

---
+++ LympiControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LympiControl
+//
+//  Created by David Bohn on 10.07.15.
+//  Copyright (c) 2015 David Bohn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
